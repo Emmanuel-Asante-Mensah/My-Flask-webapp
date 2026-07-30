dd0b48f9faa89ab32c8d582aa62fbc52f2d02467
Emmanuel Asante Mensah <emensahasante1998@gmail.com>
2026-07-30 18:32:44 +0000

    Remove unused import 'ab' from app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_bcrypt import Bcrypt
 from flask_sqlalchemy import SQLAlchemy
-ab
+
 app = Flask(__name__, static_folder='static')
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
