023035c0cc49063efe91db5e7993238c740294f7
Emmanuel Asante Mensah <emensahasante1998@gmail.com>
2026-07-30 18:35:36 +0000

    Update with latest changes

---
+++ check_db.py
@@ -0,0 +1,5 @@
+﻿import sqlite3
+conn = sqlite3.connect("instance/data.db")
+print("Tables:", conn.execute("SELECT name FROM sqlite_master WHERE type='table'").fetchall())
+print("Alembic version:", conn.execute("SELECT * FROM alembic_version").fetchall())
+conn.close()
